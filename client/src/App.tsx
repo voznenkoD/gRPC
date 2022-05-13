908c60d7840ab05d727643d136ee91b9f9c3e54d
Hernani Fernandes <hernanif1@gmail.com>
2022-05-13 15:13:16 +0200

    remove unncessary files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-import "./App.css";
 import { AuditService } from "grcp";
 import { useState } from "react";
-import { ChatMessage } from "grcp/generated/AuditService_pb";
+import { ChatMessage, ChatStory } from "grcp/generated/AuditService_pb";
 
 export const client = new AuditService.ChatAuditServiceClient(
   "http://localhost:9090"
@@ -9,28 +8,36 @@ export const client = new AuditService.ChatAuditServiceClient(
 
 export default function App() {
   const [message, setMessage] = useState("");
-
+  const [stream, setStream] = useState("");
+  const [response, setResponse] = useState<string[]>();
   const handleMessage = (event: any) => {
     setMessage(event.target.value);
   };
 
-  console.log(client);
-
-  const sendMessage = () => {
+  const sendMessage = async () => {
     const msg = new ChatMessage();
     msg.setMessage(message);
-    msg.setSender("Client");
+    msg.setSender("sender");
     msg.setChat("chat");
 
-    client.message(msg, () => {
-      console.log("Hi");
-    });
+    const callback = (
+      error: AuditService.ServiceError | null,
+      responseMessage: ChatStory | null
+    ) => {
+      console.log("responseMessage", responseMessage);
+      setResponse(responseMessage?.getStoryList());
+    };
+
+    client.message(msg, callback);
   };
 
   return (
     <>
       <textarea onChange={handleMessage} />
       <button onClick={sendMessage}>Send</button>
+      {response && <span> show response: {response}</span>}
+      <br />
+      {stream && <span>stream</span>}
     </>
   );
 }
