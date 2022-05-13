a886c1fa17e48da5440fa37e12d03132c18aed72
Hernani Fernandes <hernanif1@gmail.com>
2022-05-13 13:41:47 +0200

    add docker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,91 +1,36 @@
 import "./App.css";
-import Header from "./components/Header";
 import { AuditService } from "grcp";
-// import { User } from "./chat_pb";
-//import { ChatServiceClient } from "./chat_grpc_web_pb";
-import ChatPage from "./pages/ChatPage";
-import { useState, useRef } from "react";
+import { useState } from "react";
+import { ChatMessage } from "grcp/generated/AuditService_pb";
 
 export const client = new AuditService.ChatAuditServiceClient(
-  "http://localhost:9080",
-  null
+  "http://localhost:9090"
 );
 
 export default function App() {
-  const inputRef = useRef<any>(null);
-  const [submitted, setSubmitted] = useState(null);
-  function joinHandler() {
-    const _username = inputRef?.current?.value;
+  const [message, setMessage] = useState("");
 
-    // const user = new User();
-    // user.setId(Date.now());
-    // user.setName(_username);
+  const handleMessage = (event: any) => {
+    setMessage(event.target.value);
+  };
 
-    // client.join(user, null, (err, response) => {
-    //   if (err) return console.log(err);
-    //   const error = response.getError();
-    //   const msg = response.getMsg();
+  console.log(client);
 
-    //   if (error === 1) {
-    //     console.log(error, msg);
-    //     setSubmitted(true);
-    //     //window.alert("Username already exists.");
-    //     return;
-    //   }
-    //   window.localStorage.setItem("username", _username.toString());
-    //   setSubmitted(true);
-    //   // history.push("chatslist");
-    // });
-  }
+  const sendMessage = () => {
+    const msg = new ChatMessage();
+    msg.setMessage(message);
+    msg.setSender("Client");
+    msg.setChat("chat");
 
-  function renderChatPage() {
-    return <div>I need a BE client.</div>;
-    //return <ChatPage client={client} />;
-  }
-
-  function renderJoinPage() {
-    return (
-      <div>
-        <div>
-          <h1>Join Chat As...</h1>
-        </div>
-        <div style={{ padding: "10px 0" }}>
-          <input
-            style={{ fontSize: "1.3rem" }}
-            type="text"
-            ref={inputRef}
-            placeholder="Your username..."
-          />
-        </div>
-        <div>
-          <button
-            onClick={joinHandler}
-            style={{
-              padding: "7px 38px",
-              fontSize: "1.2em",
-              boxSizing: "content-box",
-              borderRadius: "4px",
-            }}
-          >
-            Join
-          </button>
-        </div>
-      </div>
-    );
-  }
+    client.message(msg, () => {
+      console.log("Hi");
+    });
+  };
 
   return (
     <>
-      <head>
-        <title>ChatApp</title>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <Header />
-      <div className="container">
-        <main className="main">
-          {submitted ? renderChatPage() : renderJoinPage()}
-        </main>
-      </div>
+      <textarea onChange={handleMessage} />
+      <button onClick={sendMessage}>Send</button>
     </>
   );
 }
